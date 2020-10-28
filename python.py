e437320d3133c362f8e32394a011f7475d8537ab
praveenkk-kloud9 <63289452+praveenkk-kloud9@users.noreply.github.com>
2020-10-28 12:09:35 +0530

    Create python.py

---
+++ python.py
@@ -0,0 +1,21 @@
+import unittest
+import xmlrunner
+
+# Here's our "unit".
+def IsOdd(n):
+    return n % 2 == 1
+
+# Here's our "unit tests".
+class IsOddTests(unittest.TestCase):
+
+    def testOne(self):
+        self.failUnless(IsOdd(1))
+
+    def testTwo(self):
+        self.failIf(IsOdd(2))
+
+def main():
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
+
+if __name__ == '__main__':
+    main()
